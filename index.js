59e3b1cf4810347685618b8702bd1d7439039fa2
JnBanks <joku.banks@gmail.com>
2022-04-21 23:16:26 -0700

    I added a function for badges, to no avail, and I created a markdown template for the input info to go. the software does produce a file, though not a .md file. It still needs fixing but the general content is there'

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+//function init () {
+
 const inquirer = require('inquirer');
 const fs = require('fs');
+//const generate = require('./generateReadMe.js');
+
+//const path = require('path');
 
 inquirer
   .prompt([
@@ -11,7 +16,7 @@ inquirer
     {
       type: 'input',
       name: 'description',
-      message: 'Please describe your project. What does it do, for whom, how and why?',
+      message: 'Please describe your project. Think what, how and why?',
     },
     {
       type: 'input',
@@ -21,23 +26,23 @@ inquirer
     {
       type: 'input',
       name: 'usage',
-      message: 'How does someone use your project?',
+      message: 'How do you use your project?',
     },
     {
       type: 'input',
-      name: 'contribution',
+      name: 'contributing',
       message: 'What are the contribution guidelines?',
     },
     {
       type: 'input',
       name: 'test',
-      message: 'What are the test instructions?',
+      message: 'How do you test it?',
     },
     {
       type: 'list',
       name: 'license',
-      message: 'What are the test instructions?',
-      choices: ['Academic Free License v3.0','Apache license 2.0','Artistic license 2.0','Boost Software License 1.0','Educational Community License v2.0','European Union Public License 1.1','GNU General Public License family','GNU General Public License v3.0','Open Software License 3.0']
+      message: 'What license does it have?',
+      choices: ['Apache license 2.0','Boost Software License 1.0','BSD 3-Clause License','GNU General Public License v3.0','The MIT License','Mozilla Public License 2.0']
     },
     {
         type: 'input',
@@ -51,5 +56,66 @@ inquirer
       },
   ])
   .then((data) => {
-      console.log(data);
+    
+    function badge(data) {
+        const linenseType = data.license[0];
+        let licenseString = '';
+        if (linenseType === 'Apache license 2.0') {
+            licenseString = `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
+        };
+        if (linenseType === 'Boost Software License 1.0') {
+            licenseString = `![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)`
+        };
+        if (linenseType === 'BSD 3-Clause License') {
+            licenseString = `![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)`
+        };
+        if (linenseType === 'GNU General Public License v3.0') {
+            licenseString = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+        };
+        if (linenseType === 'The MIT License') {
+            licenseString = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+        };
+        if (linenseType === 'Mozilla Public License 2.0') {
+            licenseString = `![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)`
+        };
+        return licenseString;
+    };
+    
+    const readme =
+        `# ${data.title}
+    
+        ##Description
+    
+        ##Table of Contents
+        1. [Installation](#Installation)
+        2. [Usage](#Usage)
+        3. [License](#License)
+        4. [Contributing](#Contributing)
+        5. [Tests](#Test)
+        6. [Questions](#Questions)
+    
+        ##Installation
+        ${data.installation}
+        
+        ##Usage
+        ${data.usage}
+    
+        ##License
+        ${badge(data)}
+    
+        ##Contributing
+        ${data.contributing}
+    
+        ##Tests
+        ${data.test}
+    
+        ##Questions
+        ${data.github}
+        ${data.email}`;
+
+    fs.writeFile(data.title + 'README.md', readme, (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
   })
+  
+//}
